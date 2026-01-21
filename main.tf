f1c80a8c91f43f9069fdcb2667c76fe761c1ca0a
Vansh Baliyan <vanshkr.baliyan.com>
2026-01-21 12:22:59 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  project = "striped-orbit-476513-i5"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "tt-github-demo-bucket"
+  location = "US"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
