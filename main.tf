5406749e2e694ad28f1a3f205bbcf1092bce0c35
Vansh Baliyan <vanshkr.baliyan.com>
2026-01-21 14:42:28 +0530

    new bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name                     = "tt-github-demo-bucket-vansh-1222322"
+  name                     = "tt-github-demo-bucket-vansh-1222322-new"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
